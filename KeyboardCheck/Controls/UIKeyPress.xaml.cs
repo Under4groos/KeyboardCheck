5e8b1f4ee31f9741212b72735c71ed3c4f618b33
UnderKo <hhit934@gmail.com>
2022-03-29 22:11:17 +0300

    A

---
+++ KeyboardCheck/Controls/UIKeyPress.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KeyboardCheck.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для UIKeyPress.xaml
+    /// </summary>
+    public partial class UIKeyPress : UserControl
+    {
+        System.Windows.Input.Key Key_l;
+        public System.Windows.Input.Key Key
+        {
+            get
+            {
+                return this.Key;
+            }
+            set
+             {
+                this.Key_l = value;
+                key_name.Content = value;
+            }
+        }
+        public UIKeyPress()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ KeyboardCheck/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using KeyboardCheck.Controls;
+using System;
+using System.Windows;
+
+namespace KeyboardCheck
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ThreadTimer threadTimer;
+        int count_ = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.Loaded += (o, e) =>
+            {
+                WinApiWindow winApiWindow = new WinApiWindow();
+                winApiWindow.Set(this);
+                winApiWindow.HideWin_Tab();
+                winApiWindow.MakeTransparent();
+                winApiWindow.SetMostTop(true);
+            };
+
+            threadTimer = new ThreadTimer(this);
+            threadTimer.Tick += (o, e) =>
+            {
+                x_keys.Children.Clear();
+               
+                foreach (System.Windows.Input.Key item in Enum.GetValues(typeof(System.Windows.Input.Key)))
+                {
+                    if(item == 0)
+                        continue;
+                    if (System.Windows.Input.Keyboard.IsKeyDown(item))
+                    {
+                        x_keys.Children.Add(new UIKeyPress() { Key = item });
+                        count_++;
+                    }
+                }
+               
+                
+            };
+            threadTimer.initialize();
+        }
+    }
+}
